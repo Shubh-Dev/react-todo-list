cd8f39ec1405a9bc8723cca43ca588a4aff0e4b7
Shubh <shubh.scb@gmail.com>
2022-05-11 09:38:23 +0530

    PROGRESS

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -23,8 +23,8 @@ class TodoContainer extends React.Component {
     ]
    };
 
-   handleChange = () => {
-    console.log("clicked");
+   handleChange = (id) => {
+    console.log("clicked", id);
   };
 
   render() {
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 function TodoItem(props) {
-  return <li><input type="checkbox" checked={props.todo.completed} onChange={() => props.handleChangeProps()} />{props.todo.title}</li>
+  return <li><input type="checkbox" checked={props.todo.completed} onChange={() => props.handleChangeProps(props.todo.id)} />{props.todo.title}</li>
 }
 
 export default TodoItem;
