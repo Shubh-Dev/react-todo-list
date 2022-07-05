52998e0534c399eef3fd32aae2b172df3c2d138f
Shubh <shubh.scb@gmail.com>
2022-07-05 18:15:51 +0530

    FUNCTIONAL COMPONENT ADD

--- src/classBased/components/TodoItem.js
+++ src/classBased/components/TodoItem.js
@@ -18,6 +18,10 @@ class TodoItem extends React.Component {
     }
   }
 
+  componentWillUnmount() {
+    console.log("Cleaning up...")
+  }
+
   render() {
     const completedStyle = {
       fontStyle: "italic",
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import ReactDOM from "react-dom";
 //component file
-import TodoContainer from "./components/TodoContainer";
+import TodoContainer from "./functionBased/components/TodoContainer";
 import "./App.css";
 
 ReactDOM.render(
